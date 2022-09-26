6b0dfaee9bc5abbaa422aeeeb0d72b3c6c783bd9
최의준 <100139545+EuiJunChoi@users.noreply.github.com>
2022-09-27 02:08:34 +0900

    Add files via upload
    0927 수정중

---
+++ spring-boot-qr-app/src/main/java/com/pixeltrice/springbootqrapp/SpringBootQrAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootqrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQrAppApplication.class, args);
+    }
+
+}
