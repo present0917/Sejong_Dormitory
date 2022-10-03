f14e24d68ea5b71366323b1a700a2e570ad730f2
LimGayoung <ygy920@gmail.com>
2022-10-03 13:41:01 +0900

    qrcode

---
+++ demo/src/main/java/sd_back/demo/controller/qrcode/QRCodeController.java
@@ -0,0 +1,33 @@
+package sd_back.demo.controller.qrcode;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import sd_back.demo.service.QRCodeService;
+
+@RestController
+public class QRCodeController {
+
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
+
+
+    @GetMapping(value = "/genrateAndDownloadQRCode/{codeText}/{width}/{height}")
+    public void download(
+            @PathVariable("codeText") String codeText,
+            @PathVariable("width") Integer width,
+            @PathVariable("height") Integer height)
+            throws Exception {
+        QRCodeService.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
+    }
+
+    @GetMapping(value = "/genrateQRCode/{codeText}/{width}/{height}")
+    public ResponseEntity<byte[]> generateQRCode(
+            @PathVariable("codeText") String codeText,
+            @PathVariable("width") Integer width,
+            @PathVariable("height") Integer height)
+            throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(QRCodeService.getQRCodeImage(codeText, width, height));
+    }
+}
