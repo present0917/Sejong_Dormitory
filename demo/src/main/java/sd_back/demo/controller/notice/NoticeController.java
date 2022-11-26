f6e79613935f83092b01a1d30bcf6965808cce73
LimGayoung <ygy920@gmail.com>
2022-11-26 15:38:07 +0900

    update notice

--- demo/src/main/java/sd_back/demo/controller/notice/NoticeController.java
+++ demo/src/main/java/sd_back/demo/controller/notice/NoticeController.java
@@ -2,7 +2,6 @@ package sd_back.demo.controller.notice;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,8 +12,8 @@ import sd_back.demo.service.MemberService;
 import sd_back.demo.service.NoticeService;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -31,7 +30,6 @@ public class NoticeController {
         if (noticeList.isEmpty()) {
             return new ResponseEntity<>(null, HttpStatus.OK);
         }
-
         return new ResponseEntity<>(noticeList, HttpStatus.OK);
     }
 
@@ -45,8 +43,33 @@ public class NoticeController {
     @ResponseBody
     @GetMapping("/notice/{id}")
     public ResponseEntity getNoticeById(@PathVariable int id) { //공지사항 상세 내용
-        Notice noticeById = noticeService.findNoticeById(id);
+        Optional<Notice> noticeById = noticeService.findNoticeById(id);
 
-        return new ResponseEntity(noticeById, HttpStatus.OK);
+        if (noticeById.isEmpty()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity(noticeById.get(), HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @PutMapping("/notice/{id}") //공지사항 업데이트
+    public ResponseEntity updateNotice(@PathVariable int id, @Valid @RequestBody NoticeForm form, BindingResult bindingResult) {
+        Optional<Notice> notice = noticeService.updateNotice(id, form.title, form.content, form.dateTime);
+
+        if (notice.isEmpty()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity(notice, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @DeleteMapping("/notice/{id}")
+    public ResponseEntity deleteNotice(@PathVariable int id) { //공지사항 삭제
+        if(!noticeService.deleteNotice(id)){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
